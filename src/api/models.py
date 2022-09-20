fb41eee677b95198c66fe20e0dae224273037543
Fernando Monnot <fermonnot@gmail.com>
2022-09-20 17:34:51 -0400

    issue #2

--- src/api/models.py
+++ src/api/models.py
@@ -23,8 +23,6 @@ class Products (db.Model):
     description = db.Column(db.String(120), unique=True, nullable=False)
     laboratory = db.Column(db.String(120), unique=False, nullable=False)
     price = db.Column(db.Float, unique=False, nullable=False)
-    # url_image = db.Column(db.String(50), unique=False, nullable=False) ----- para que funcione debo borrar todos los datos 
-    # de la base de datos
 
     def serialize(self):
         return {
